e78790bf0565bd65e50b127290b37237c9cca921
Lilia Markham <lilia.markham@causes.com>
2012-03-17 23:30:13 -0700

    total_invested and percent_ownership

--- app/models/house.rb
+++ app/models/house.rb
@@ -3,4 +3,12 @@ class House < ActiveRecord::Base
   has_many :members, :through => :memberships, :source => :user
   has_many :investments
   has_many :expenses
+
+  def total_invested
+    investments.map(&:amount).sum
+  end
+
+  def percent_ownership(user)
+    investments.where(:user_id => user.id).sum.to_f / total_invested
+  end
 end
