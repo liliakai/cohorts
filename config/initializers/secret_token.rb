4ad7faa3e1238b4c5b1000813ee6cb32641ba2ce
Lilia Markham <lilia.markham@causes.com>
2012-03-17 21:05:33 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cohorts::Application.config.secret_token = 'a6b6b805676e48003841ee4b1cc548010473cc7fd2087b1324e897420a25c1f39f93baacd1704f6a5c9f654a7d6c5c861bb42103f74cdb3897d37485ed2a23b6'
