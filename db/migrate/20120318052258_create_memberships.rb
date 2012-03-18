491a519573f38839bfb318055a276df15d3a2b9b
Lilia Markham <lilia.markham@causes.com>
2012-03-17 22:37:12 -0700

    Redo migration

--- db/migrate/20120318052258_create_memberships.rb
+++ db/migrate/20120318052258_create_memberships.rb
@@ -1,6 +1,8 @@
 class CreateMemberships < ActiveRecord::Migration
   def change
     create_table :memberships do |t|
+      t.integer :user_id
+      t.integer :house_id
       t.boolean :occupant
       t.boolean :owner
       t.timestamps
