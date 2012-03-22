715735f24645d7278d39b1d7b56cedbf67040ed6
Lilia Markham <lilia.markham@causes.com>
2012-03-22 00:53:16 -0700

    fix named scope

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -2,6 +2,6 @@ class Investment < ActiveRecord::Base
   belongs_to :user
   belongs_to :house
 
-  named_scope :by_user, lambda {|user| {:conditions => {:user_id => user.id}}}
-  named_scope :in_house, lambda {|house| {:conditions => {:house_id => house.id}}}
+  scope :by_user, lambda {|user| {:conditions => {:user_id => user.id}}}
+  scope :in_house, lambda {|house| {:conditions => {:house_id => house.id}}}
 end
