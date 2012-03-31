a546cc41d20a537e984b66e574739c427b26338c
Lilia Markham <lilia.markham@causes.com>
2012-03-31 01:08:24 -0700

    limited equity on an investment is equal to the original amount plus annual inflation.

--- app/models/investment.rb
+++ app/models/investment.rb
@@ -2,6 +2,31 @@ class Investment < ActiveRecord::Base
   belongs_to :user
   belongs_to :house
 
+  attr_accessor :amount
+
   scope :by_user, lambda {|user| {:conditions => {:user_id => user.id}}}
   scope :in_house, lambda {|house| {:conditions => {:house_id => house.id}}}
+
+  def limited_equity
+    years = Time.now.year - created_at.year
+    0..years.inject(amount) do |total, year_number|
+      total * (1.0 + inflation_rate(year_number))
+    end
+  end
+
+  def inflation_rate(num_years)
+    time = created_at + (num_years).years
+    percent_change cpi(time_start), cpi(time + 1.year)
+  end
+
+  def percent_change(val1, val2)
+    # todo (val2 - val1)/ val1
+    0.04
+  end
+
+  def cpi(time_start, time_end)
+    # todo
+    100
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   has_many :memberships
   has_many :houses, :through => :memberships
   has_many :investments
+
+  def equity_in(house)
+    total = investments.in_house(house).map(&:limited_equity).sum
+  end
+
 end
