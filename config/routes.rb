80d7afd66f234c3acb5959fe13320d4ecf6b8dbd
Lilia Markham <lilia.markham@causes.com>
2012-04-03 01:43:31 -0700

    add routes for new controllers

--- config/routes.rb
+++ config/routes.rb
@@ -56,5 +56,5 @@ Cohorts::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-  resources :houses
+  resources :houses, :investments, :expenses
 end
